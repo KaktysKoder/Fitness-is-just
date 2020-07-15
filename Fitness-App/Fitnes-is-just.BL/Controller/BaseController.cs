2d9f2c16195678c47703adeb487610c5ff6b96d2
Rimuru <kaktysKoder@gmail.com>
2020-07-15 09:22:37 +0400

    abstract class BaseController : Реализована общая логика для сериализации и десериализации.

---
+++ Fitness-App/Fitnes-is-just.BL/Controller/BaseController.cs
@@ -0,0 +1,45 @@
+﻿using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Fitnes_is_just.BL.Controller
+{
+    /// <summary>
+    /// Общая логика для сериализации и десериализации.
+    /// </summary>
+    public abstract class BaseController
+    {
+        /// <summary>
+        /// Принемает данные необходимые для сериализазии.
+        /// </summary>
+        /// <param name="fileName" > Имя файла в который будет сериализованны данные.</param>
+        /// <param name="item"     > Данные для сериализазии.                        </param>
+        protected void Save<T>(string fileName, object item)
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+
+            using (FileStream fs = new FileStream(fileName, FileMode.OpenOrCreate))
+            {
+                formatter.Serialize(fs, item);
+            }
+        }
+
+        /// <summary>
+        /// Десериализация данных из файла fileName
+        /// </summary>
+        /// <param name="fileName"> Имя файла из которого произойдёт десериализация данных.</param>
+        /// <returns></returns>
+        protected T Load<T>(string fileName)
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+
+            using (FileStream fs = new FileStream(fileName, FileMode.OpenOrCreate))
+            {
+                if (fs.Length > 0 && formatter.Deserialize(fs) is T items)
+                {
+                    return items;
+                }
+                else return default;
+            }
+        }
+    }
+}
--- Fitness-App/Fitnes-is-just.BL/Controller/EatingController.cs
+++ Fitness-App/Fitnes-is-just.BL/Controller/EatingController.cs
@@ -1,11 +1,33 @@
-﻿using System;
+﻿using Fitnes_is_just.BL.Model;
+using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace Fitnes_is_just.BL.Controller
 {
+    /// <summary>
+    /// Контроллер приёма пищей.
+    /// </summary>
     public class EatingController
     {
+        private readonly User user;
 
+        public EatingController(User user)
+        {
+
+            this.user = user ?? throw new ArgumentNullException("Пользователь не может быть пустым.", nameof(user));
+            Foods = GetAllFoods();
+        }
+
+        #region Props
+        /// <summary>
+        /// Список еды.
+        /// </summary>
+        public List<Food> Foods { get; }
+        #endregion
+
+        private List<Food> GetAllFoods()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
