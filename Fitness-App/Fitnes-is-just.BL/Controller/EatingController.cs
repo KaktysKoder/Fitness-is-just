f11bebc256c95251329495f5b805e3a490548077
Rimuru <kaktysKoder@gmail.com>
2020-07-15 08:48:39 +0400

    Реализован класс продукта питания.

---
+++ Fitness-App/Fitnes-is-just.BL/Controller/EatingController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fitnes_is_just.BL.Controller
+{
+    public class EatingController
+    {
+
+    }
+}
---
+++ Fitness-App/Fitnes-is-just.BL/Model/Food.cs
@@ -0,0 +1,50 @@
+﻿namespace Fitnes_is_just.BL.Model
+{
+    /// <summary>
+    /// Продукт питания.
+    /// </summary>
+    public class Food
+    {
+        public Food(string name) : this(name, 0, 0, 0, 0) { }
+
+        public Food(string name, double callorie, double proteins, double fats, double carbohydrates)
+        {
+            //TODO: Сделать проверки.
+
+            Name          = name;
+            Callorie      = callorie      / 100.0;
+            Proteins      = proteins      / 100.0;
+            Fats          = fats          / 100.0;
+            Carbohydrates = carbohydrates / 100.0;
+        }
+
+        #region Props
+        /// <summary>
+        /// Наименование продукта.
+        /// </summary>
+        public string Name { get; }
+
+        /// <summary>
+        /// Калории.
+        /// </summary>
+        public double Callorie { get; }
+
+        /// <summary>
+        /// Белки.
+        /// </summary>
+        public double Proteins { get; }
+
+        /// <summary>
+        /// Жиры.
+        /// </summary>
+        public double Fats { get; }
+
+        /// <summary>
+        /// Углеводы.
+        /// </summary>
+        public double Carbohydrates { get; }
+        #endregion
+
+        public override string ToString() => Name;
+    }
+}
